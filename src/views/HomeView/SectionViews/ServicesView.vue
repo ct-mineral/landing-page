a49416f2fe3493f6e3f9eb79f0e6b2f331083fa7
Jardson Alan <jardsonalan132@gmail.com>
2026-01-31 20:28:02 -0300

    feat: Adiciona links dinâmicos aos cartões de serviços na página inicial

--- src/views/HomeView/SectionViews/ServicesView.vue
+++ src/views/HomeView/SectionViews/ServicesView.vue
@@ -3,12 +3,10 @@
   import CardSectionComponent from '@/components/CardSectionComponent.vue';
 
   const informationsCards = [
-    { id: 1, title: 'Administração de Amostras', description: 'lorem impsum', image: 'images/diretores.png' },
-    { id: 2, title: 'Análises', description: 'lorem impsum', image: 'images/diretores.png' },
-    { id: 3, title: 'Preparação de Amostras', description: 'lorem impsum', image: 'images/diretores.png' },
-    { id: 4, title: 'Testes Físicos', description: 'lorem impsum', image: 'images/diretores.png' },
-    { id: 5, title: 'Serviços Metalúrgicos', description: 'lorem impsum', image: 'images/diretores.png' },
-    { id: 6, title: 'Serviços no Local', description: 'lorem impsum', image: 'images/diretores.png' },
+    { id: 1, title: 'Administração de Amostras', description: 'lorem impsum', image: 'images/diretores.png', link: '/servicos/administracao-de-amostras' },
+    { id: 2, title: 'Análises', description: 'lorem impsum', image: 'images/diretores.png', link: '/servicos/analises' },
+    { id: 3, title: 'Preparação de Amostras', description: 'lorem impsum', image: 'images/diretores.png', link: '/servicos/preparacao-de-amostras' },
+    { id: 4, title: 'Testes Físicos', description: 'lorem impsum', image: 'images/diretores.png', link: '/servicos/testes-fisicos' },
   ]
 </script>
 
@@ -22,12 +20,13 @@
       <v-col cols="12">
         <v-slide-group>
           <v-slide-group-item>
-            <v-container class="d-flex justify-center ga-5 ga-md-12 px-0" fluid>
-              <div v-for="i in informationsCards" :key="i.id">
+            <v-container v-for="i in informationsCards" :key="i.id" fluid>
+              <div class="d-flex justify-center ga-5 ga-md-12 px-0">
                 <CardSectionComponent
                   :title-props="i.title"
                   :description-props="i.description"
                   :image-props="i.image"
+                  :link-props="i.link"
                 />
               </div>
             </v-container>
