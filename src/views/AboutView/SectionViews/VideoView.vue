5917a293d39a3352e14b93e43bb2209637a5ce2e
Jardson Alan <jardsonalan132@gmail.com>
2026-02-10 21:53:23 -0300

    feat: Implementa funcionalidade de vídeo play e play button

--- src/views/AboutView/SectionViews/VideoView.vue
+++ src/views/AboutView/SectionViews/VideoView.vue
@@ -1,11 +1,35 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const showVideo = ref(false)
+
+const playVideo = () => {
+  showVideo.value = true
+}
+</script>
 
 <template>
   <v-container>
     <v-row>
       <v-col cols="12">
-        <div class="video-container">
-          <iframe src="https://www.youtube.com/embed/Wit4LgJ7uy8?si=Q1SVtEkjNeAQSs4k" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen loading="lazy"></iframe>
+        <div class="video-container d-flex justify-center align-center" @click="playVideo">
+          <template v-if="!showVideo">
+            <img
+              src="/images/logos/logo_ctm.svg"
+              alt="Vídeo institucional"
+              class="thumb w-50 h-50"
+            />
+            <div class="play-button">▶</div>
+          </template>
+
+          <iframe
+            v-else
+            src="https://www.youtube-nocookie.com/embed/Wit4LgJ7uy8?autoplay=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="autoplay; encrypted-media; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
       </v-col>
     </v-row>
@@ -13,10 +37,28 @@
 </template>
 
 <style scoped>
-.video-container iframe {
+.video-container {
+  position: relative;
+  width: 100%;
+  aspect-ratio: 16 / 9;
+  cursor: pointer;
+}
+
+.video-container iframe,
+.video-container .thumb {
   width: 100%;
   height: 100%;
   border: 0;
-  aspect-ratio: 16 / 9;
+}
+
+.play-button {
+  position: absolute;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 64px;
+  color: white;
+  background: rgba(0, 0, 0, 0.4);
 }
 </style>
