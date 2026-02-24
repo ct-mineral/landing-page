2703c8eb9935dfafbd21cdae89759d38745c0e35
Jardson Alan <jardsonalan132@gmail.com>
2026-02-24 15:39:20 -0300

    feat: Adiciona plugin do Vuetify para otimização

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import '@/assets/base.css'
 
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
@@ -15,8 +13,6 @@ import router from './router'
 const app = createApp(App)
 
 const vuetify = createVuetify({
-  components,
-  directives,
   icons: {
     defaultSet: 'mdi',
   },
