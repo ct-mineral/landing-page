5161fcc643a530fc1b7e612fe783c460545bda54
Jardson Alan <jardsonalan132@gmail.com>
2026-07-17 15:43:43 -0300

    feat(router): Adiciona scroll behavior customizado

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,24 @@ import HomeView from '@/views/HomeView/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+
+    if (to.path !== from.path) {
+      return { top: 0, left: 0 }
+    }
+
+    return false
+  },
   routes: [
     {
       path: '/',
